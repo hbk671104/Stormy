795d31ae1226f1646685ad71cad000dc0c50c082
hbk671104 <hbk671104@gmail.com>
2015-03-08 16:43:49 -0400

    First commit

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	private let apiKey = "f038070bd7ace19b10621d8380c2fb5d"
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+		let forecastURL = NSURL(string: "40.816526,-77.886265", relativeToURL: baseURL)
+		
+		let sharedSession = NSURLSession.sharedSession()
+		let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+			
+			if (error == nil) {
+				let dataObject = NSData(contentsOfURL: location)
+				let weatherDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+				
+				
+			}
+				
+		})
+		downloadTask.resume()
+		
 	}
 
 	override func didReceiveMemoryWarning() {
