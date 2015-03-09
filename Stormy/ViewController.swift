c87acafbed1b81694ea069b7ddea19a5b6864731
hbk671104 <hbk671104@gmail.com>
2015-03-08 22:28:53 -0400

    Added current location lookup

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
 	@IBOutlet weak var iconView: UIImageView!
 	@IBOutlet weak var currentTimeLabel: UILabel!
@@ -17,36 +18,21 @@ class ViewController: UIViewController {
 	@IBOutlet weak var precipitationLabel: UILabel!
 	@IBOutlet weak var summaryLabel: UILabel!
 	
-	
 	private let apiKey = "f038070bd7ace19b10621d8380c2fb5d"
+	let locationManager = CLLocationManager()
+	let sharedSession = NSURLSession.sharedSession()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
-		let forecastURL = NSURL(string: "40.816526,-77.886265", relativeToURL: baseURL)
-		
-		let sharedSession = NSURLSession.sharedSession()
-		let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
-			
-			if (error == nil) {
-				let dataObject = NSData(contentsOfURL: location)
-				let weatherDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
-				
-				let currentWeather = Current(weatherDictionary: weatherDictionary)
-				dispatch_async(dispatch_get_main_queue(), { () -> Void in
-					self.temperatureLabel.text = "\(currentWeather.temperature!)"
-					self.iconView.image = currentWeather.icon!
-					self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is"
-					self.humidityLabel.text = "\(currentWeather.humidity)"
-					self.precipitationLabel.text = "\(currentWeather.precipProbability)"
-					self.summaryLabel.text = "\(currentWeather.summary)"
-				})
-			}
-				
-		})
-		downloadTask.resume()
+		// Location manager config
+		self.locationManager.requestWhenInUseAuthorization()		
+		if CLLocationManager.locationServicesEnabled() {
+			self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+			self.locationManager.delegate = self
+			self.locationManager.startUpdatingLocation()
+		}
 		
 	}
 
@@ -55,6 +41,74 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+		
+		// Optional binding
+		if let location = manager.location {
+			
+			let coordinate = location.coordinate
+			
+			// Stop location update, just to save some battery
+			self.locationManager.stopUpdatingLocation()
+			
+			// Configure requestURL based on lat and long
+			var requestURL = configureRequestURL(coordinate.latitude, long: coordinate.longitude)
+			
+			retrieveWeatherData(requestURL)
+			
+		}
+		
+	}
+	
+	func configureRequestURL(lat: Double, long: Double) -> NSURL {
+		
+		let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+		let forecastURL = NSURL(string: "\(lat),\(long)", relativeToURL: baseURL)
+		
+		return forecastURL!
+		
+	}
+	
+	func retrieveWeatherData(forecastURL: NSURL?) {
+		
+		let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+			
+			if (error == nil) {
+				
+				let dataObject = NSData(contentsOfURL: location)
+				let weatherDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+				
+				let currentWeather = Current(weatherDictionary: weatherDictionary)
+				dispatch_async(dispatch_get_main_queue(), { () -> Void in
+					
+					self.updateWeatherInterface(
+						"\(currentWeather.temperature!)",
+						image: currentWeather.icon!,
+						currentTime: "At \(currentWeather.currentTime!) it is",
+						humidity: "\(currentWeather.humidity)",
+						precip: "\(currentWeather.precipProbability)",
+						summary: "\(currentWeather.summary)")
+					
+				})
+				
+			}
+			
+		})
+		
+		downloadTask.resume()
+	
+	}
+	
+	func updateWeatherInterface(temp: String, image: UIImage, currentTime: String, humidity: String, precip: String, summary: String) {
+		
+		self.temperatureLabel.text = temp
+		self.iconView.image = image
+		self.currentTimeLabel.text = currentTime
+		self.humidityLabel.text = humidity
+		self.precipitationLabel.text = precip
+		self.summaryLabel.text = summary
+		
+	}
 
 }
 
